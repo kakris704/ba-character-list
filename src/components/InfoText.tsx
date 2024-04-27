63df50c7d41566fc424353b8fe233bcee3245ff7
kakris704 <kakris704@gmail.com>
2024-04-28 02:32:17 +0900

    色の調整

--- src/components/InfoText.tsx
+++ src/components/InfoText.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 const InfoText = ({ children, info, sx = {}}: {children: any, info: any, sx?:any}) => {
   return (
     <Stack direction='row' sx={sx}>
-        <div style={{width: 5, backgroundColor: 'gray', borderRadius: 10}}/>
+        <div style={{width: 5, backgroundColor: '#bbbbbb', borderRadius: 10}}/>
         <Stack direction='column'>
             <Typography variant='body2' sx={{mt: 'auto', pl:0.5, color: 'gray'}}>{info}</Typography>
             <Typography variant='h6' sx={{float: 'right', pl: 0.4}}>{children}</Typography>
